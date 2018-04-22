6f986fe063a681f97468d5084306c7231340d4e1
Rodrigo Barreto <rcabarreto@gmail.com>
2018-04-21 21:17:47 -0300

    added commander support and default values

--- bannerGenerator.js
+++ bannerGenerator.js
@@ -1,15 +1,26 @@
+#!/usr/bin/env node
+
 'use strict';
+
+const options = require('commander');
 
 const image = require('./lib/imageManipulator');
 
-let output = {
-  width: 1920,
-  height: 1280,
-  quality: 80,
-  blur: 50
-};
+function number(number){
+  return parseInt(number);
+}
 
-image.processAll(output).then(result => {
+options
+  .version('1.0.0')
+  .option('-w, --width [width]', 'width of output image [width]', number, 1920)
+  .option('-h, --height [height]', 'height of output image', number, 1280)
+  .option('-q, --quality [quality]', 'quality of output image', number, 80)
+  .option('-b, --blur [blur]', 'blur of bg bars [50]', number, 50)
+  .option('-f, --folder [folder]', 'Path of the source folder', '/images/src/')
+  .parse(process.argv);
+
+
+image.processAll(options).then(result => {
   console.log(result);
 }, err => {
 
